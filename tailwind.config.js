b0f0c6e085cf49e8aa0a3f55f037679dfc3f028d
Sikal Sikal <sikal.sikal.ss@gmail.com>
2021-10-01 09:11:00 +0100

    Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: "jit",
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        nosh_blue: {
+          DEFAULT: "#30BCED",
+          dark: "#282356",
+          light: "#F5FCFF",
+          app: "#EAF8FD",
+          signIn: "#395d69"
+        }
+      },
+      fontFamily : {
+        hurme: " 'hurme' "
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
